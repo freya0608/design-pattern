fb9e0798b7b4f7cf7f6ff4ed52f6f85cfb4197ee
freya0608 <freya001@yeah.net>
2018-12-09 16:54:30 +0800

    环境搭建

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # design-pattern
 design-pattern-js
+
+ ``` npm i webpack-dev-server html-webpack-plugin --save-dev ```
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by freya on 2018/12/9.
+ * https://www.jetbrains.com/help/webstorm/file-template-variables.html
+ 动画callback只支持1.x版本的TransitionGroup
+ */
+
+console.log('hshsh49898z')
